105efff232c116faf79d71cd566a0b1c4db39912
Hollow <fhr2001@qq.com>
2020-09-13 00:01:01 +0800

    完善了静态分配顺序表文件

--- Linear List/Sequence List/静态分配顺序表.cpp
+++ Linear List/Sequence List/静态分配顺序表.cpp
@@ -1,3 +1,10 @@
+#include<cstring>
+#include<cstdio>
+#include<algorithm>
+#include<iostream>
+
+using namespace std;
+
 #define MaxSize 10//定义最大长度
 typedef int ElemType ;//这里以int类型为案例
 
@@ -5,3 +12,15 @@ typedef struct{
 	ElemType data[MaxSize];//用静态的“数组”存放数据元素
 	int length;//顺序表当前的长度
 }Sequencelist;
+
+void Initlist(Sequencelist &L){
+	for(int i=0;i<MaxSize;i++)	L.data[i]=0;
+	L.length=0;
+}
+
+int main(){
+	Sequencelist L;
+	Initlist(L);
+	
+	return 0;
+}
